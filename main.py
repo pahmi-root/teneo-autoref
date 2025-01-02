bf18394039882304b465b5b58dae24b157a9f003
Hanzou Urushihara <61415393+im-hanzou@users.noreply.github.com>
2025-01-02 10:30:05 +0700

    Update main.py fix bugs
    Update main.py fix bugs

--- main.py
+++ main.py
@@ -370,7 +370,7 @@ class TeneoAutoref:
                         return True
                     
             except Exception as e:
-                log_message(self.current_num, self.total, f"Activation error, retrying...", "warning")
+                log_message(self.current_num, self.total, f"Activation error. Retrying, just wait...", "warning")
                 continue
         
     def check_user_onboarded(self, access_token):
